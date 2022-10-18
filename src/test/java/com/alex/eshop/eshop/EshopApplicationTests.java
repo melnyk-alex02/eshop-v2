db5ba30366e98a5f23299689ae4dd89a85ec6d18
Sasha Melnyk <alex.p.melnyk@gmail.com>
2022-10-19 00:45:13 +0300

    Initial commit

---
+++ src/test/java/com/alex/eshop/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.eshop.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
